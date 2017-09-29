bf022b0133a8dd86c5cc968b90191f5083ad42ed
Ridwan Hasanah <sun.riseagain@yahoo.com>
2017-09-29 15:26:27 +0700

    mengubah date picker agar bisa memilih tahun secara bebas

--- assets/js/daftarUmroh.js
+++ assets/js/daftarUmroh.js
@@ -1,11 +1,15 @@
 jQuery(document).ready(function(){
 
 	/*===== Date Picker Start =====*/
-jQuery(".datePicker").datepicker({dateFormat:"yy-mm-dd"});
-/*===== Date Picker End =====*/
+	jQuery(".datePicker").datepicker({
+		dateFormat:"yy-mm-dd",
+		changeMonth: true,
+		changeYear: true,
+		yearRange: "1900:2222"});
+	/*===== Date Picker End =====*/
 
-jQuery('#rdelete').click(function(){
-	alert('reload');
-	location.reload();
-});
+	jQuery('#rdelete').click(function(){
+		alert('reload');
+		location.reload();
+	});
 })
